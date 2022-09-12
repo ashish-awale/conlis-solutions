a5ef28d701b93c94ddc93a4e0977c9cee5fc0a98
ashish-awale <ashish.awale@conlistech.com>
2022-09-12 11:01:25 +0530

    modify Company.js file to update page

--- src/pages/company/Company.js
+++ src/pages/company/Company.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import "./Company.css"
-import company from "../../images/company/company.jpg"
-import Footer from '../../components/Footer'
+// import company from "../../images/company/company.jpg"
+// import Footer from '../../components/Footer'
+import About from '../about/About'
 
 const Company = () => {
     return (
         <>
-            <div>
+            {/* <div>
                 <div className="company-image">
                     <img src={company} alt="company-top" />
                 </div>
                 <div className="service-text">
                     <h2> Company</h2>
                 </div>
-            </div>
-            <Footer />
+            </div> */}
+            <About />
+            {/* <Footer /> */}
         </>
     )
 }
